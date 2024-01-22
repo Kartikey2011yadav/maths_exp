616044c3c23b80ee7a8b4f35bc3d13df0b5a33c0
Kartikey2011yadav <nykay10968@gmail.com>
2024-01-22 16:36:32 +0530

    gsl library test : successful

--- src/maths_exp.c
+++ src/maths_exp.c
@@ -1,10 +1,21 @@
 #include <stdlib.h>
 #include "maths_exp.h"
 #include "gsl_math.h"
+#include "gsl_diff.h"
+
+double f (double x, void * params)
+{
+    (void)(params); /* avoid unused parameter warning */
+    return pow (x, 10);
+}
 
 FFI_PLUGIN_EXPORT intptr_t sum(intptr_t a, intptr_t b) {
-
-    return GSL_IS_ODD(a+b); }
+    gsl_function  F;
+    double result, abserr;
+    F.function = &f;
+    F.params = 0;
+    gsl_diff_central(&F, 6.0, &result, &abserr);
+    return result; }
 
 FFI_PLUGIN_EXPORT intptr_t sum_long_running(intptr_t a, intptr_t b) {
   // Simulate work.
